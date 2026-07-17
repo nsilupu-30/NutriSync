e79b5989b129a606b9ccaacbf35d58da78ded2d8
ManuelCarrasco28 <josemanuelcarrascomillan@gmail.com>
2026-07-17 07:35:55 -0500

    feat: agregar configuracion dinamica de CSRF_TRUSTED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,12 @@ ALLOWED_HOSTS = config(
 if DEBUG:
     ALLOWED_HOSTS.append("*")
 
+CSRF_TRUSTED_ORIGINS = config(
+    "CSRF_TRUSTED_ORIGINS",
+    default="http://localhost:8000,http://127.0.0.1:8000,http://localhost:3000",
+    cast=lambda v: [s.strip() for s in v.split(",")]
+)
+
 
 # ─── Apps instaladas ──────────────────────────────────────────────────────────
 
